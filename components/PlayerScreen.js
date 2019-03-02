159f61a38487d7fec96342d442f70b445716b9de
dgg6b <dgg6b@virginia.edu>
2019-03-02 17:20:54 -0500

    added the play screen

--- components/PlayerScreen.js
+++ components/PlayerScreen.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import {View, TouchableOpacity, Image, StyleSheet} from 'react-native'
+import {View, TouchableOpacity, Image, StyleSheet, Text} from 'react-native'
 import {Audio} from 'expo'
+import Slider from "react-native-slider";
+
 
 export default class PlayerScreen extends React.Component{
     constructor(props){
@@ -8,68 +10,128 @@ export default class PlayerScreen extends React.Component{
         var {navigation} = this.props
         this.state = {
             podID: navigation.getParam('pod', '0'), 
-            isPlaying: false
+            image: "https://www.cs.virginia.edu/~dgg6b/Mobile/Images/PodCastImage3.png",
+            title: "Another Great Piece",
+            subTitle: "Written by freeMusicArchive.org",
+            soundLoaded: false,
         }
     }
 
     componentDidMount(){
         const soundObject = new Audio.Sound()
-        soundObject.loadAsync(require("../assets/tunes3.mp3")).then(
+        soundObject.loadAsync(require("../assets/tunes3.mp3")).then( ()=>{
             this.setState({
                 soundLoaded: true,
-                soundObject: soundObject
+                soundObject: soundObject,
+                soundObjectStatus: JSON.parse(soundObject._lastStatusUpdate)
             })
-        )
+            soundObject.setOnPlaybackStatusUpdate(()=>{this.onPlaybackStatusUpdate()})
+        })
     }
 
+     onPlaybackStatusUpdate(){
+        this.setState({
+            soundObjectStatus: JSON.parse(this.state.soundObject._lastStatusUpdate)
+        })
+    }
+
+    async rewind(time){
+        try {
+            await this.state.soundObject.setPositionAsync(this.state.soundObjectStatus.positionMillis - time)
+        }catch(err){
+            console.log(err)
+        }
+    }
+ 
     async toggleSound(){
-        if(this.state.soundLoaded && !this.state.isPlaying){
+        if(this.state.soundObjectStatus === null || this.state.soundLoaded && !this.state.soundObjectStatus.isPlaying){
             try {
-                    await this.state.soundObject.playAsync();
-                    this.setState({
-                        isPlaying: true
-                    })
+                    if(this.state.soundObjectStatus !== null && this.state.soundObjectStatus.didJustFinish){
+                        await this.state.soundObject.setPositionAsync(0)
+                        await this.state.soundObject.playAsync()
+                    }else{
+                        await this.state.soundObject.playAsync()
+                    }    
                 } catch (error) {
                     console.log("error with sound")
                 }
         }else{
             try {
                 await this.state.soundObject.pauseAsync();
-                this.setState({
-                    isPlaying: false
-                })
             } catch (error) {
                 console.log("error with sound")
             }
         }
-    
     }
 
+
     render(){
+       
+       
+
         return(
             <View style={styles.container}>
-                <View style={ styles.container} >
-                   
-                    {!this.state.isPlaying ? (
-                         <TouchableOpacity
-                         onPress={
-                             ()=>{this.toggleSound()}
-                         }
-                         >
-                        <Image 
-                        source={require('../assets/playButton.png')} />
+                <Image
+                style={styles.featurePlayerImage}
+                 source={{uri: this.state.image}} />
+              
+              { this.state.soundLoaded && this.state.soundObjectStatus !== null ?(
+                  <View> 
+                    <Slider  
+                        style={styles.slider}
+                        value = {this.state.soundObjectStatus.positionMillis/1000}
+                        maximumValue = {this.state.soundObjectStatus.durationMillis/1000}
+                        minimumTrackTintColor = "#FFF"
+                        thumbTintColor = "#FFF"
+                        minimumValue ={0}
+                    />
+                    <View style={styles.sliderContainer} style={{paddingTop: 10, flexDirection: 'row', justifyContent:'space-between'}}>
+                        <Text style={{color: "#fff"}}> {this.state.soundObjectStatus.positionMillis/1000}</Text> 
+                        <Text style={{color: "#fff"}}> {this.state.soundObjectStatus.durationMillis/1000}</Text> 
+                    </View> 
+                </View> 
+                ):
+                (
+                    <Slider  
+                    style={styles.slider}
+                    value = {0}
+                    maximumValue = {10}
+                    minimumTrackTintColor = "#FFF"
+                    thumbTintColor = "#FFF"
+                    minimumValue ={0}
+                />
+                )
+                }
+            <View>
+                <Text style={styles.title}> {this.state.title}</Text>
+                <Text style={styles.subTitle}> {this.state.subTitle}</Text>
+            </View>
+
+                {  this.state.soundLoaded ? (
+                    <View style={ styles.controlContainer} >
+                        <TouchableOpacity onPress={() =>{this.rewind(15000)}} >
+                            <Image source={require('../assets/Backward15.png')} />
                         </TouchableOpacity>
-                    ):(
-                        <TouchableOpacity
-                         onPress={
-                             ()=>{this.toggleSound()}
-                         }
-                         >
-                        <Image 
-                        source={require('../assets/pauseButton.png')} />
+                        
+                        <TouchableOpacity onPress={ ()=>{this.toggleSound()}}>
+                            
+                        {this.state.soundObjectStatus === null || !this.state.soundObjectStatus.isPlaying? (
+                            <Image 
+                            source={require('../assets/playButton.png')} />
+                        ):(
+                        
+                            <Image 
+                            source={require('../assets/pauseButton.png')} />
+                        )}
                         </TouchableOpacity>
-                    )}
-                </View>
+                            <TouchableOpacity onPress={() =>{this.rewind(-15000)}} >
+                                <Image 
+                                    source={require('../assets/Forward15.png')} />
+                        </TouchableOpacity>
+                        </View>
+                    ):(<View/>)
+                    }
+                
             </View>
         )
     }
@@ -83,13 +145,29 @@ const styles = StyleSheet.create({
         flex:1, 
         backgroundColor: "#000D0C",
         flexDirection: 'column',
-        justifyContent: 'center',
+        justifyContent: 'space-evenly',
         alignItems:'center'
 
     },
-    ControlContainer:{
+    subTitle:{
+        color: "#D8D8D8"
+    },
+    title:{
+        fontSize: 22,
+        color: "#FFFFFF"
+    },
+    featurePlayerImage:{
+        width: 315, 
+        height: 315,
+    },
+    slider:{
+        width: 315, 
+        height: 5,
+    },
+
+    controlContainer:{
+        width: 250, 
         flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems:'center'
+        justifyContent: 'space-between',
     }
 })
